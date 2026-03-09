4206b25706131ef633fa587e08cbbe7c99d33836
王万杰 <wanjiewang@kugou.net>
2026-03-09 09:36:28 +0800

    Initial Commit

---
+++ HostsEditorTests/HostsEditorTests.swift
@@ -0,0 +1,17 @@
+//
+//  HostsEditorTests.swift
+//  HostsEditorTests
+//
+//  Created by VanJay on 2026/3/9.
+//
+
+import Testing
+@testable import HostsEditor
+
+struct HostsEditorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
